109e842d6cf897bed07f710a8696ff3f56909ce1
TanyaGolovina <golovina.tanya2011@yandex.ru>
2017-04-14 17:52:57 +0300

    Custom functions and array

--- index.php
+++ index.php
@@ -1,12 +1,56 @@
 <?php
 
-$a=5;
+//$a=5;
 
-print 'Hellooo';
+//print 'Hellooo';
 
-print $a;
-var_dump($a,'Tanya');
+//print $a;
+//var_dump($a,'Tanya');
 
-define('type','String');
-print type;
+//define('type','String');
+//print type;
+$nm='Rita';
 
+function Hello($name = 'Guest')
+{
+    $mess="Hello ".$name."!<br>";
+    if ($name === 'Guest') 
+    {
+       //$mess = $mess.'<a href="#">Login</a>';
+         $mess .='<a href="#">Login</a>';
+    }
+    elseif($name = 'Rita')
+    {
+       $mess = "How are you,".$name."?";
+    }
+    else 
+    {
+     $mess .='<a href="#">Log out</a>';
+    }
+    return $mess;
+    
+}
+
+//print Hello();
+print Hello($nm);
+
+//index array
+//$arr = array();
+$arr = [1 => 1, 2, 3];
+$arr[] = 5;
+$arr[] = 55;
+unset($arr[3]);
+
+var_dump($arr);
+
+print $arr[2];
+
+
+
+//assoc array
+$lang = [
+        'backend' => ['PHP', 'Python'],
+        'frontend' => ['JavaScript'],
+        ];
+print_r($lang);
+print $lang['frontend'][0];
